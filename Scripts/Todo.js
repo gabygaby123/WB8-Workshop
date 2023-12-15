dec10f9d777aa637f974a841b6e225b1513b1f0e
jaquellsensi <146892715+jaquellsensi@users.noreply.github.com>
2023-12-15 14:37:28 -0700

    scripts v2

---
+++ Scripts/Todo.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', async function () {
+    const apiUrl = 'http://localhost:8083/api/categories';
+    const dropdown = document.getElementById('dropdown');
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        data.forEach(item => {
+            const option = document.createElement('option');
+            option.value = item.id; 
+            option.text = item.name; 
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+});
